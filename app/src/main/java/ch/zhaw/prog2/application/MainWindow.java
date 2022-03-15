c55b34260cb410d4d540808e7e1a07b8c3fea808
RodRobotics <eduardo.rodriguez813@gmail.com>
2022-03-15 21:05:53 +0100

    FXML done

---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
@@ -0,0 +1,26 @@
+package ch.zhaw.prog2.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        openMainWindow(primaryStage);
+    }
+    private void openMainWindow(Stage stage) {
+    }
+
+
+
+}
